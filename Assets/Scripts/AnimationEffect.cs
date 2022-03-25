6ef963531332e45c872a15f7d2caa880725d5ac2
FourmiSushi <neko@fourmisushi.soy>
2022-03-25 17:12:13 +0900

    スクリプトを追加

---
+++ Assets/Scripts/AnimationEffect.cs
@@ -0,0 +1,72 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+// ReSharper disable UnusedMember.Global
+
+namespace FourmiSushi.Animation
+{
+    /// <summary>
+    /// エフェクトを再生するコンポーネント
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public class AnimationEffect : MonoBehaviour
+    {
+        private PlayableGraph _graph;
+        private AnimationMixerPlayable _mixer;
+        private AnimationClipPlayable _currentPlayable;
+
+        private void Awake()
+        {
+            _graph = PlayableGraph.Create();
+            _mixer = AnimationMixerPlayable.Create(_graph, 1, true);
+            AnimationPlayableOutput.Create(_graph, "output", GetComponent<Animator>()).SetSourcePlayable(_mixer);
+            _graph.Play();
+        }
+
+        /// <summary>
+        /// エフェクトを再生する
+        /// </summary>
+        /// <param name="animationClip">再生するアニメーション</param>
+        /// <param name="loop">ループさせるかどうか</param>
+        /// <returns>エフェクト終了時に終了するUniTask</returns>
+        public UniTask Play(AnimationClip animationClip, bool loop = false)
+        {
+            if (_currentPlayable.IsValid())
+                _currentPlayable.Destroy();
+            _graph.Disconnect(_mixer, 0);
+            _currentPlayable = AnimationClipPlayable.Create(_graph, animationClip);
+
+            if (!loop)
+            {
+                _currentPlayable.SetDuration(animationClip.length);
+            }
+
+            _mixer.ConnectInput(0, _currentPlayable, 0);
+
+            _mixer.SetInputWeight(0, 1);
+
+            return UniTask.WaitUntil(() => _currentPlayable.IsDone(), PlayerLoopTiming.LastUpdate);
+        }
+
+        /// <summary>
+        /// エフェクトを停止する
+        /// </summary>
+        public void Stop()
+        {
+            _currentPlayable.SetDone(true);
+        }
+
+        /// <summary>
+        /// エフェクトをゲームオブジェクトに貼り付ける
+        /// </summary>
+        /// <param name="g">貼り付ける対象のゲームオブジェクト</param>
+        /// <param name="offset">対象からずらす距離</param>
+        public void AttachToGameObject(GameObject g, Vector2 offset = default)
+        {
+            Transform transform1;
+            (transform1 = transform).SetParent(g.transform);
+            transform1.localPosition = Vector3.zero + (Vector3)offset;
+        }
+    }
+}
---
+++ Assets/Scripts/AnimationEffectPlayer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+// ReSharper disable UnusedMember.Global
+
+namespace FourmiSushi.Animation
+{
+    /// <summary>
+    /// エフェクトを再生するためのコンポーネント
+    /// </summary>
+    public class AnimationEffectPlayer : MonoBehaviour
+    {
+        private AnimationEffect _skillEffectPrefab;
+
+        protected void Awake()
+        {
+            _skillEffectPrefab = Resources.Load<AnimationEffect>("Prefabs/アニメーション/SkillEffect");
+        }
+
+
+        /// <summary>
+        /// エフェクトを単発で再生する
+        /// </summary>
+        /// <param name="anim">再生するアニメーション</param>
+        /// <param name="position">再生する座標</param>
+        /// <param name="scale">再生サイズ</param>
+        /// <param name="angle">再生角度</param>
+        /// <returns>エフェクト終了時に終了するUniTask</returns>
+        public UniTask PlayEffect(AnimationClip anim, Vector2 position, Vector2 scale, float angle = 0)
+        {
+            var effect = Instantiate(
+                _skillEffectPrefab,
+                transform
+            );
+
+            var transform1 = effect.transform;
+            transform1.position = position;
+            transform1.localScale = scale;
+            transform1.Rotate(Vector3.forward, angle);
+
+            var eff = effect.Play(anim);
+
+            return eff.ContinueWith(() => Destroy(effect.gameObject));
+        }
+
+        /// <summary>
+        /// エフェクトをループさせて時間制限なしで再生 自分で止めたい時用
+        /// ゲームオブジェクトが返るのでエフェクトを移動させたい時も使う
+        /// </summary>
+        /// <param name="anim">再生するアニメーション</param>
+        /// <param name="position">再生する座標</param>
+        /// <param name="scale">再生サイズ</param>
+        /// <param name="angle">再生角度</param>
+        /// <returns>エフェクト .Stop()でエフェクトを終了できる</returns>
+        public AnimationEffect PlayEffectLoop(AnimationClip anim, Vector2 position, Vector2 scale, float angle = 0)
+        {
+            var effect = Instantiate(
+                _skillEffectPrefab,
+                transform
+            );
+
+            var transform1 = effect.transform;
+            transform1.position = position;
+            transform1.localScale = scale;
+            transform1.Rotate(Vector3.forward, angle);
+
+            effect.Play(anim, true).ContinueWith(() => Destroy(effect.gameObject));
+
+            return effect;
+        }
+
+        /// <summary>
+        /// エフェクトをループさせて指定した時間再生
+        /// </summary>
+        /// <param name="anim">再生するアニメーション</param>
+        /// <param name="timeSpan">再生する時間</param>
+        /// <param name="position">再生する座標</param>
+        /// <param name="scale">再生サイズ</param>
+        /// <param name="angle">再生角度</param>
+        /// <returns>指定した時間が経つと終了するUniTask</returns>
+        public UniTask PlayEffectFor(AnimationClip anim, TimeSpan timeSpan, Vector2 position, Vector2 scale,
+            float angle = 0)
+        {
+            var effect = Instantiate(
+                _skillEffectPrefab,
+                transform
+            );
+
+            var transform1 = effect.transform;
+            transform1.position = position;
+            transform1.localScale = scale;
+            transform1.Rotate(Vector3.forward, angle);
+
+            effect.Play(anim, true);
+
+            return UniTask.Delay(timeSpan, DelayType.DeltaTime).ContinueWith(() => Destroy(effect.gameObject));
+        }
+    }
+}
